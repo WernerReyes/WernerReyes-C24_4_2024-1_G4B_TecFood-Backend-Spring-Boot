31ad8dc4db3e4c3be6a25765374941f96692f656
Werner Reyes <reyespilcowerner@gmail.com>
2024-06-25 01:00:48 -0500

    Bugs fixed

--- src/main/java/com/backend/app/models/dtos/orderDish/UpdateOrderDishStatusDto.java
+++ src/main/java/com/backend/app/models/dtos/orderDish/UpdateOrderDishStatusDto.java
@@ -1,17 +1,18 @@
 package com.backend.app.models.dtos.orderDish;
 
-import com.backend.app.models.dtos.NotNullAndNotEmpty;
 import com.backend.app.persistence.enums.EOrderDishStatus;
 import lombok.*;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateOrderDishStatusDto {
-    @NotNullAndNotEmpty(message = "Order dish ID is required")
+    @NotNull(message = "Order dish ID is required")
     private Long orderDishId;
 
-    @NotNullAndNotEmpty(message = "Status is required")
+    @NotNull(message = "Status is required")
     private EOrderDishStatus status;
 
 }
